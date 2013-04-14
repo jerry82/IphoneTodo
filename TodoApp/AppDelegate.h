33edddc61eb5b0f8362d7a0c0745ee0853ffb4d7
Jerry <darkmouse@Jerrys-MacBook-Air.local>
2013-04-14 16:23:56 -0400

    Initial Commit

---
+++ TodoApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TodoApp
+//
+//  Created by Jerry on 14/4/13.
+//  Copyright (c) 2013 Jerry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TodoApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TodoApp
+//
+//  Created by Jerry on 14/4/13.
+//  Copyright (c) 2013 Jerry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
